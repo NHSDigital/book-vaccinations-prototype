58e9502410aaa95f71f21d22985a0aa36f16ec3d
Joanna Lumley <jolumley@Joannas-MacBook-Air.local>
2024-08-21 10:00:46 +0100

    first push:RSV

---
+++ app/routes.js
@@ -0,0 +1,37 @@
+// External dependencies
+const express = require('express');
+
+const router = express.Router();
+
+// Add your routes here - above the module.exports line
+
+module.exports = router;
+
+// RSV routes //
+
+router.get(/nhsNumberRadio/, function (req, res) {
+    if (req.query.radioGroup === "yes" ) {
+        res.redirect('nhs-no');
+        }
+    else {
+        res.redirect('name');
+        }
+    });
+
+    router.get(/confirmationMessages/, function (req, res) {
+        if (req.query.radioGroup === "yes" ) {
+            res.redirect('contact-details');
+            }
+        else {
+            res.redirect('contact-no');
+            }
+        });
+
+        router.get(/areYouSure/, function (req, res) {
+            if (req.query.radioGroup === "yes" ) {
+                res.redirect('booking-complete');
+                }
+            else {
+                res.redirect('contact-details');
+                }
+            });
