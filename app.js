e887722cd6a7ba2cee133361c711c4091b820b6f
Colin Rotherham <work@colinr.com>
2026-03-06 14:12:02 +0000

    Complete NHS prototype kit v8 upgrade

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const { join } = require('node:path')
-
 const NHSPrototypeKit = require('nhsuk-prototype-kit')
 
 // Local dependencies
@@ -9,26 +7,29 @@ const filters = require('./app/filters')
 const locals = require('./app/locals')
 const routes = require('./app/routes')
 
-const SERVICE_NAME = config.serviceName
+const viewsPath = [
+  'app/views/'
+]
 
-// Set configuration variables
-const port = parseInt(process.env.PORT, 10) || 2000
+const entryPoints = [
+  'app/assets/sass/main.scss',
+  'app/assets/javascript/*.js'
+]
 
-const viewsPath = join(__dirname, 'app/views/')
+async function init() {
+  const prototype = await NHSPrototypeKit.init({
+    serviceName: config.serviceName,
+    buildOptions: {
+      entryPoints,
+    },
+    viewsPath,
+    routes,
+    locals,
+    filters,
+    sessionDataDefaults,
+  });
 
-const prototype = NHSPrototypeKit.init({
-  serviceName: SERVICE_NAME,
-  routes: routes,
-  locals: locals,
-  sessionDataDefaults: sessionDataDefaults,
-  viewsPath: viewsPath,
-  buildOptions: {
-    entryPoints: ['app/assets/sass/main.scss']
-  }
-})
-
-for (const [name, filter] of Object.entries(filters())) {
-  prototype.nunjucks.addFilter(name, filter)
+  prototype.start(config.port);
 }
 
-prototype.start(port)
+init();
